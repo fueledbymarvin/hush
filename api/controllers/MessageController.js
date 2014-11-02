fc7f9f46382a3fb477d64a8ca20f5675fb9b029e
Calvin <calvin.chan.h@gmail.com>
2014-11-01 23:17:44 -0400

    Routing works

--- api/controllers/MessageController.js
+++ api/controllers/MessageController.js
@@ -7,40 +7,44 @@
 
 module.exports = {
     messageReceived: function(req, res) {
-        var sender = req.body('From'),
-            twilioReceivingNumber = req.body('To'),
-            body = req.body('Body');
+        var sender = req.param('From'),
+            twilioReceivingNumber = req.param('To'),
+            body = req.param('Body');
+        console.log(sender, twilioReceivingNumber, body);
         
         // Find User who sent msg 
-        User.findOrCreate({phone: sender}, {phone: sender}, function (err, users) {
+        User.findOrCreate({phoneNumber: sender}, {phoneNumber: sender}, function (err, users) {
             if (err) {
-                return cb(err);
+                res.send(err);
             } else {
                 user = users.isArray ? users[0] : users;
+                console.log(user);
             }
 
             if (twilio.isMainNumber(twilioReceivingNumber)){
                 // This creates a message thread
-                var regex = /^@[0-9]{10} /;
-                var receiver;
-                if (receiver = regex.exec(body)) {
-                    user.credits++;
-                    user.save(function (err,user) {
-                        var content = body.replace(regex, '');
-                        User.findOrCreate({phone: receiver},{phone: receiver}).populate('threads').exec(function(err, recipients) {
-                            if (err) {
-                                return twilio.sendError({
-                                    from: twilio.MAIN_NUMBER,
-                                    to: sender,
-                                    msg: "Sorry, something went wrong"
-                                });
-                            } else {
-                                recipient = recipients.isArray() ? recipients[0] : recipients;
-                            }
-                            recipient.newMessage({
-                                from: user,
-                                msg: content 
+                var regex = /^[0-9]{10}/;
+                var receiver = regex.exec(body);
+                console.log(receiver);
+                if (receiver[0]) {
+                    var content = body.replace(regex, '');
+                    User.findOrCreate({phoneNumber: receiver},{phoneNumber: receiver}).populate('threads').exec(function(err, recipients) {
+                        console.log("HELLO");
+                        if (err) {
+                            console.log(err);
+                            res.send(err);
+                            return twilio.sendError({
+                                from: twilio.MAIN_NUMBER,
+                                to: sender,
+                                msg: "Sorry, something went wrong"
                             });
+                        } else {
+                            recipient = recipients.isArray ? recipients[0] : recipients;
+                            console.log(recipient);
+                        }
+                        recipient.newMessage({
+                            from: user,
+                            msg: content 
                         });
                     });
                 } else {
@@ -50,7 +54,7 @@ module.exports = {
                         msg: "Doesn't specify a receiving number."
                     });
                 }
-            } else if(twilio.RECEIVING_NUMBERS.contains(twilioReceivingNumber)) {
+            } else if(twilio.RECEIVING_NUMBERS.indexOf(twilioReceivingNumber) != -1) {
                 // This is a response to a message thread
             }
         });
