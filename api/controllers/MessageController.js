8ec90841f2d7abdedffcc92b147282779d494797
Calvin <calvin.chan.h@gmail.com>
2014-11-02 01:54:54 -0400

    There's a random error when you first init the database but like whatever fuck it ship it

--- api/controllers/MessageController.js
+++ api/controllers/MessageController.js
@@ -10,44 +10,45 @@ module.exports = {
         var sender = req.param('From'),
             twilioReceivingNumber = req.param('To'),
             body = req.param('Body');
-        console.log(sender, twilioReceivingNumber, body);
         
         // Find User who sent msg 
         User.findOrCreateByPhone({phoneNumber: sender}, function (err, users) {
+            console.log(users);
             if (err) {
+                console.log("ERROR");
                 res.send(err);
+                return;
             } else {
                 user = users.isArray ? users[0] : users;
-                console.log(user);
             }
 
             if (twilio.isMainNumber(twilioReceivingNumber)){
                 // This creates a message thread
-                var regex = /^[0-9]{10}/;
-                var receiver = regex.exec(body);
-                console.log(receiver);
-                if (receiver[0]) {
+                var regex = /^[0-9]{10}\ /;
+                var receiver = regex.exec(body)[0];
+                if (receiver) {
                     var message = body.replace(regex, '');
                     User.findOrCreateByPhone({phoneNumber: receiver}, function(err, recipients) {
-                        console.log("HELLO");
                         if (err) {
-                            console.log(err);
-                            res.send(err);
-                            return twilio.sendError({
+                            twilio.sendError({
                                 from: twilio.MAIN_NUMBER,
                                 to: sender,
                                 msg: "Sorry, something went wrong"
                             });
+                            res.send(err);
+                            return;
                         } else {
                             recipient = recipients.isArray ? recipients[0] : recipients;
-                            console.log(recipient);
                         }
-                        console.log("HELLO");
                         Message.create({
                             from: user,
                             content: message,
                             to: recipient
                         }, function(err,m){
+                            if (err) {
+                                return;
+                            }
+                            console.log("About to add to queue");
                             recipient.addToQueue(m);
                             user.addSendCredit();
                             res.send("Probably worked");
